877d5c6d749974dfaf2fc3aa76471d94236de05d
Esteban Jimenez <estebandjs7@gmail.com>
2023-08-15 18:19:39 -0400

    Initial Commit

---
+++ Tateti_App_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tateti_App_ios
+//
+//  Created by Esteban Jiménez on 2023-08-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
